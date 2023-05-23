6ad06a4533fa87f5406582bd89a70c4ddde6d485
jongyoung3 <74577670+jongyoung3@users.noreply.github.com>
2023-05-23 13:08:44 +0900

    Update search_google_main(last).py

--- search_google_main(last).py
+++ search_google_main(last).py
@@ -2,92 +2,30 @@ import os
 import googlemaps
 import requests
 
-api_key = ''
+api_key = 'YOUR_API_KEY'
 map_clinet = googlemaps.Client(api_key)
 
 result_list = [] #최종 결과물 리스트
 result_ex=[] #최종 결과물 리스트 전 단계
 
 # chat-gpt 로 받아올 데이터
-# 도쿄 애니메이션 여행
-# search_locations =['Ghibli Museum(Mitaka, Tokyo, Japan)', 'Akihabara(Chiyoda City, Tokyo, Japan)', 
-#                    'Odaiba(Minato City, Tokyo, Japan)', 'Nakano Broadway(Nakano, Tokyo, Japan)', 
-#                    'Pokemon Center Tokyo(Chuo City, Tokyo, Japan)', 'J-World Tokyo(Ikebukuro, Tokyo, Japan)', 
-#                    'Animate Ikebukuro(Toshima City, Tokyo, Japan)', 'Tokyo Anime Center(Chiyoda City, Tokyo, Japan)', 
-#                    'Otome Road(Ikebukuro, Tokyo, Japan)', 'Shinjuku Wald 9(Shinjuku City, Tokyo, Japan)']
-# chat-gpt 로 받아올 데이터
-# search_locations = ['Ginza', 'Shibuya 109', 'Harajuku',
-#                    'Omotesando', 'Tokyo Solamachi',
-#                    'Mega Don Quijote Shibuya',
-#                    'Kiddy Land Harajuku', 'Kitkat Chocolatory',
-#                    'Tsukiji Outer Market', 'Odaiba VenusFort']
-# search_locations=['Tower of London', 'Westminster Abbey', 
-#                   'Buckingham Palace', 'Churchill War Rooms', 
-#                   'The British Museum', 'Canterbury Cathedral', 
-#                   'Stonehenge', 'Roman Baths', "St. Paul's Cathedral", 
-#                   'Edinburgh Castle']
-# data_locations=['Tower of London(London, UK)', 'Westminster Abbey(London, UK)', 
-#                   'Buckingham Palace(London, UK)', 'Churchill War Rooms(London, UK)', 
-#                   'The British Museum(London, UK)', 'Canterbury Cathedral(Canterbury, UK)', 
-#                   'Stonehenge(Amesbury, UK)', 'Roman Baths(Bath, UK)', "St. Paul's Cathedral(London, UK)", 
-#                   'Edinburgh Castle(Edinburgh, UK)']
-data_locations=[
-    #도쿄 애니메이션 여행
-    ['Ghibli Museum(Mitaka, Tokyo, Japan)', 'Akihabara(Chiyoda City, Tokyo, Japan)', 
-    'Odaiba(Minato City, Tokyo, Japan)', 'Nakano Broadway(Nakano, Tokyo, Japan)', 
-    'Pokemon Center Tokyo(Chuo City, Tokyo, Japan)', 'J-World Tokyo(Ikebukuro, Tokyo, Japan)', 
-    'Animate Ikebukuro(Toshima City, Tokyo, Japan)', 'Tokyo Anime Center(Chiyoda City, Tokyo, Japan)', 
-    'Otome Road(Ikebukuro, Tokyo, Japan)', 'Shinjuku Wald 9(Shinjuku City, Tokyo, Japan)']
-
-    #영국 역사 여행
-    ,['Tower of London(London, UK)', 'Westminster Abbey(London, UK)', 
-    'Buckingham Palace(London, UK)', 'Churchill War Rooms(London, UK)', 
-    'The British Museum(London, UK)', 'Canterbury Cathedral(Canterbury, UK)', 
-    'Stonehenge(Amesbury, UK)', 'Roman Baths(Bath, UK)', 
-    "St. Paul's Cathedral(London, UK)", 'Edinburgh Castle(Edinburgh, UK)']
-
-    #한국 자연휴식 여행
-    ,['Nami Island(Chuncheon, Gangwon-do, South Korea)', 'Seoraksan National Park(Sokcho-si, Gangwon-do, South Korea)',
-    'Jeju Island(Jeju Province, South Korea)', 'Jirisan National Park(Gurye-gun, Jeollanam-do, South Korea)', 
-    'Bukhansan National Park(Goyang-si, Gyeonggi-do, South Korea)', 'Seorak Waterpia(Sokcho-si, Gangwon-do, South Korea)', 
-    'Gyeongju(Gyeongsangbuk-do, South Korea)', 'Hallasan National Park(Jeju Province, South Korea)', 
-    'Odaesan National Park(Pyeongchang, Gangwon-do, South Korea)', 'Gapyeong(Gyeonggi-do, South Korea)']
-
-    #제주도 여행
-    ,['Seongsan Ilchulbong(Seongsan-eup, Seogwipo, Jeju Island, South Korea)', 
-    'Jeju Folk Village(Pyoseon-myeon, Seogwipo, Jeju Island, South Korea)', 
-    'Manjanggul Cave(Gujwa-eup, Jeju City, Jeju Island, South Korea)', 
-    'Jusangjeolli Cliff(Daepo-dong, Seogwipo, Jeju Island, South Korea)', 
-    'Museum of Sex and Health(Aewol-eup, Jeju City, Jeju Island, South Korea)', 
-    'Spirited Garden(Gangjeong-dong, Seogwipo, Jeju Island, South Korea)',
-    'Hallasan National Park(Jeju-do, South Korea)', 'Jeju Loveland(Daejeong-eup, Seogwipo, Jeju Island, South Korea)', 
-    'Beaches of Jeju Island(Jeju-do, South Korea)', 'Jeju Teddy Bear Museum(2829-10, Saekdal-dong, Seogwipo, Jeju Island, South Korea)']
-
-    #미국 자동차 역사 여행
-    ,['Ford Piquette Avenue Plant(Detroit, Michigan, USA)', 'The Henry Ford Museum(Dearborn, Michigan, USA)', 
-    'Gilmore Car Museum(Hickory Corners, Michigan, USA)', 'Studebaker National Museum(South Bend, Indiana, USA)', 
-    "Walt Disney's Barn(Griffith Park, Los Angeles, California, USA)", 'Petersen Automotive Museum(Los Angeles, California, USA)',
-    "Bonnie and Clyde's Death Car(Primm, Nevada, USA)", 'Route 66 Museum(Kingman, Arizona, USA)', 
-    'National Automobile Museum(Reno, Nevada, USA)', 'The Auburn Cord Duesenberg Automobile Museum(Auburn, Indiana, USA)']
-
-    #남미 오지탐험 여행
-    ,['Machu Picchu(Cusco, Peru)', 'Galapagos Islands(Ecuador)', 'Iguazu Falls(Misiones Province, Argentina/Brazil)',
-    'Torres del Paine National Park(Magallanes Region, Chile)', 'Cartagena(Bolívar Department, Colombia)', 
-    'La Paz(Bolivia)', 'Rio de Janeiro(Rio de Janeiro State, Brazil)', 'Salar de Uyuni(Potosi and Oruro Departments, Bolivia)',
-    'Atacama Desert(Antofagasta Region, Chile)', 'Amazon Rainforest(Brazil, Peru, Colombia)']
-    ]
+search_locations=['Tower of London', 'Westminster Abbey', 
+                  'Buckingham Palace', 'Churchill War Rooms', 
+                  'The British Museum', 'Canterbury Cathedral', 
+                  'Stonehenge', 'Roman Baths', "St. Paul's Cathedral", 
+                  'Edinburgh Castle']
 
 min_rating = 0  # 최소 평점
 min_reviews = 0  # 최소 리뷰 수
 
-for search_locations in data_locations:
 
-    for locations in search_locations:
-        response = map_clinet.places(query=locations) # 데이터를 api로 보냄
-        #print(response['results'])
-        destination = []
-        if(response['results']!=[]): #검색데이터 결과가 빈 리스트로 오는 경우(=검색결과가 없을때)를 걸러줌
-            if('rating' in response['results'][0]): # 인덱스 번호에 따라 영업점 나오는 듯. 기준으로만 일단 만듬
+# 지도에 데이터 보낼 때 좌표로 보내는 걸로 코드 바꾸기(원한다면)
+for locations in search_locations:
+    response = map_clinet.places(query=locations) # 데이터를 api로 보냄
+    #print(response['results'])
+    destination = []
+    if(response['results']!=[]): #검색데이터 결과가 빈 리스트로 오는 경우(=검색결과가 없을때)를 걸러줌
+        if('rating' in response['results'][0]): # 인덱스 번호에 따라 영업점 나오는 듯. 기준으로만 일단 만듬
                 # '0'->지역 외의 장소, 평점이 있는 것
                 # [0,'name','rating','user_ratings_total']
                 destination.append(0)
@@ -95,7 +33,7 @@ for search_locations in data_locations:
                 destination.append(response['results'][0]['rating'])
                 destination.append(response['results'][0]['user_ratings_total'])
                     
-            else:
+        else:
                 # '1'->지역이름, 여기서 지역이름에 관광명소를 평점높고 리뷰수 많은거 1개 가져오면 됨. 단, 없는건 건너뛰고
                 # [1,'locations',('name','rating','user_ratings_total')]
                 destination.append(1)
@@ -156,7 +94,7 @@ for search_locations in data_locations:
                         destination.append(locations) # 검색한 지역이름
                         destination.extend(attractions)
                 
-        else: #검색데이터 결과가 빈 리스트로 오는 경우(=검색결과가 없을때) 텍스트 검색으로 다시 찾기
+    else: #검색데이터 결과가 빈 리스트로 오는 경우(=검색결과가 없을때) 텍스트 검색으로 다시 찾기
             url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={locations}&key={api_key}"
             response = requests.get(url)
             data = response.json()
@@ -180,8 +118,8 @@ for search_locations in data_locations:
                 
         #print(destination)
         
-        if(destination!=[0] and destination!=[1]): # destination 안에 0 혹은 1만 있는 경우가 아니면
-            result_ex.append(destination)
+    if(destination!=[0] and destination!=[1]): # destination 안에 0 혹은 1만 있는 경우가 아니면
+            result_ex.append(destination)    
 
 result_list.extend(result_ex)
 print(result_list)
