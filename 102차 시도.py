0d58790cb5c761ca1453c5f476f9d7743aea4585
junseokho <118727064+junseokho@users.noreply.github.com>
2023-05-29 13:43:09 +0900

    Add files via upload

---
+++ 102차 시도.py
@@ -0,0 +1,119 @@
+import googlemaps
+import folium
+import webbrowser
+import requests
+from geopy.distance import geodesic
+from datetime import datetime
+from googlemaps import convert
+
+
+
+#n=int(input("지점 개수: "))
+lat=[]
+lon=[]
+distance=[]
+site=['Tokyo Disneyland(Urayasu, Chiba Prefecture, Japan)', 'Osaka Castle(Osaka, Japan)', 'Fushimi Inari Shrine(Kyoto, Japan)', 'Hiroshima Peace Memorial Park(Hiroshima, Japan)', 'Kinkaku-ji Temple(Kyoto, Japan)', 'Nagoya Castle(Nagoya, Aichi Prefecture, Japan)', 'Mount Fuji(Yamanashi Prefecture, Japan)', 'Universal Studios Japan(Osaka, Japan)', 'Shinjuku Gyoen National Garden(Shinjuku City, Tokyo, Japan)', 'Gunkanjima Island(Nagasaki, Japan)']
+n=len(site)
+'''
+for i in range(n):
+    temp=input("site: ")
+    site.append(temp)
+'''
+# API 키
+API_KEY = 'AIzaSyBHDdUYyuLXz5mwKb7pBDZRscDp2dUIcg0'
+
+gmaps = googlemaps.Client(key=API_KEY)
+
+# Geocoding API 엔드포인트
+geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+    
+for i in range(n):
+    
+    wanted=site[i]
+    
+    # 요청 파라미터
+    params = {
+        'address': wanted,
+        'key': API_KEY,
+    }
+
+    # geocode API 요청 보내기
+    response = requests.get(geocode_url, params=params)
+
+    # 응답 처리, 좌표 구하기
+    if response.status_code == 200:
+        data = response.json()
+        if data['status'] == 'OK':
+            # 첫 번째 결과의 좌표 얻기
+            location = data['results'][0]['geometry']['location']
+            lat.append(location['lat'])
+            lon.append(location['lng'])
+        else:
+            print('장소를 찾을 수 없습니다.')
+    else:
+        print('API 요청 실패')
+
+origin=(lat[0],lon[0])
+# 지도 생성
+map = folium.Map(location=origin, zoom_start=13)
+
+for i in range(n-1):
+    origin=(lat[i],lon[i])
+    destination=(lat[i+1],lon[i+1])
+    
+    
+
+
+    directions_response=gmaps.directions(origin,destination)
+    
+    if len(directions_response) > 0 and 'legs' in directions_response[0]:
+        route = directions_response[0]['legs'][0]
+
+        points = []
+        for step in route['steps']:
+            start = (step['start_location']['lat'], step['start_location']['lng'])
+            end = (step['end_location']['lat'], step['end_location']['lng'])
+            points.extend([start, end])
+
+        if i == 0:
+            folium.Marker([lat[i], lon[i]], popup=site[i]).add_to(map)
+
+        folium.Marker([lat[i+1], lon[i+1]], popup=site[i+1]).add_to(map)
+
+        # 경로 그리기
+        folium.PolyLine(points, color='blue', weight=5).add_to(map)
+    else:
+        print(f"No directions found for the route from {site[i]} to {site[i+1]}")
+'''
+    # 경로 추출
+    route = directions_response[0]['legs'][0]
+
+    # 경로 좌표 추출
+    points = []
+    for step in route['steps']:
+        start = (step['start_location']['lat'], step['start_location']['lng'])
+        end = (step['end_location']['lat'], step['end_location']['lng'])
+        points.extend([start, end])
+
+
+    if i==0:
+        folium.Marker([lat[i], lon[i]], popup=site[i]).add_to(map)
+            
+    folium.Marker([lat[i+1], lon[i+1]], popup=site[i+1]).add_to(map)
+    
+    # 경로 그리기
+    folium.PolyLine(points, color='blue', weight=5).add_to(map)
+'''
+
+# HTML 파일로 저장
+map.save('route.html')
+
+# 생성된 HTML 파일 열기
+webbrowser.open('route.html')
+'''
+layer=gmaps.directions.Directions(origin,destination,mode="driving",avoid="ferries",departure_time=datetime.now())
+fig.add_layer(layer)
+display(fig)
+'''
+
