da3285d467d1de91cfb9be1d75538f061269b024
jongyoung3 <74577670+jongyoung3@users.noreply.github.com>
2023-05-30 13:53:13 +0900

    Update search.py

--- search.py
+++ search.py
@@ -520,6 +520,6 @@ def search(input_search_locations=[], retry=0):
         return search(input_search_locations,retry)
 
 #TEST
-res_sol=search(['Tokyo(Kanto Region, Japan)'])
-print(res_sol)
+# res_sol=search(['Tokyo(Kanto Region, Japan)'])
+# print(res_sol)
 #['Tokyo(Kanto Region, Japan)', 'Kyoto(Kansai Region, Japan)', 'Osaka(Kansai Region, Japan)', 'Hiroshima(Chugoku Region, Japan)', 'Nara(Kansai Region, Japan)']
