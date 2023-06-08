96ec57788f68bf3e6cccd7d2f5948d522e9a164e
JeongAYoon <118725721+JeongAYoon@users.noreply.github.com>
2023-06-08 15:24:01 +0900

    Add files via upload
    버튼들 이미지 변경, 기타 텍스트 변경

--- GUI.py
+++ GUI.py
@@ -74,7 +74,7 @@ class Ui_MainWindow(QMainWindow):
     #삭제 관련, 교체 관련
     #삭제 버튼(check to delete) 크기 지정
         self.deleteButton = QtWidgets.QPushButton(self.centralwidget)
-        self.deleteButton.setGeometry(QtCore.QRect(1050, 65, 131, 31))
+        self.deleteButton.setGeometry(QtCore.QRect(1040, 65, 142, 31))
     #쓰레기통 그려진 버튼 크기 지정, 숨김
         self.trashCan = QtWidgets.QPushButton(self.centralwidget)
         self.trashCan.setGeometry(QtCore.QRect(1150, 65, 31, 31))
@@ -89,7 +89,8 @@ class Ui_MainWindow(QMainWindow):
         self.cancelBtn.hide()
     #리뷰끼리 위치 바꾸는 변경버튼 크기 지정
         self.changeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.changeButton.setGeometry(QtCore.QRect(590, 65, 70, 31))
+        self.changeButton.setGeometry(QtCore.QRect(590, 65, 112, 30))
+        self.changeButton.setText("위치 변경")
     #변경버튼 아이콘 지정
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("./change.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
@@ -181,7 +182,7 @@ class Ui_MainWindow(QMainWindow):
         MainWindow.setWindowTitle("TripWithGPT")
         self.PsTitle.setText("P.S.")
         self.PsContents.setText("일본은 최첨단 기술뿐만 아니라 고대 문화가 풍부한 대조적인 나라로 가득합니다. 상징적인 랜드마크와 번화한 도시부터 산과 바다의 고요한 자연의 아름다움까지, 이 매혹적인 나라에는 모두를 위한 무언가가 있습니다. 전통과 현대의 독특한 조화를 경험할 준비를 하시고 여행 중에 맛있는 현지 요리를 맛보는 것도 잊지 마세요!")
-        self.deleteButton.setText("Check to delete")
+        self.deleteButton.setText("다른 지역으로 교체")
         self.cancelBtn.setText("Cancel")
         self.menuabout.setTitle("메뉴")
         self.actionAbout.setText("About")
@@ -214,7 +215,7 @@ class Ui_MainWindow(QMainWindow):
 
     #Qpixmap으로 no_imgs에 사진 넣기
         pixmap = QPixmap("no_image.png")
-        pixmap = pixmap.scaled(130,115)
+        pixmap = pixmap.scaled(130,100)
 
         for i in range(0,5,1):
     #리뷰 제목들 위치, 폰트, text 세팅
@@ -227,8 +228,8 @@ class Ui_MainWindow(QMainWindow):
             self.contents[i].setWordWrap(True)
             self.contents[i].setAlignment(QtCore.Qt.AlignTop)
     #리뷰 이미지, 별점들, 체크박스 위치 및 text 세팅, 체크박스 숨김
-            self.imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 130))
-            self.no_imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 130))
+            self.imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 100))
+            self.no_imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 100))
             self.no_imgs[i].setPixmap(pixmap)
 
             self.reviewPoints[i].setGeometry(QtCore.QRect(144, 44+195*i, 51, 17))
@@ -240,19 +241,19 @@ class Ui_MainWindow(QMainWindow):
             self.checkBoxes[i].hide()
             self.LandmarksName[i].setGeometry(QtCore.QRect(145, 64+195*i, 418, 16))
     #리뷰 내용들 처음엔 안보이게, 버튼도 비활성화
-        for i in range(0,5,1):
-            self.names[i].hide()
-            self.contents[i].hide()
-            self.reviews[i].hide()
-            self.reviewPoints[i].hide()
-            self.reviewStars[i].hide()
-            self.imgs[i].hide()
-            self.no_imgs[i].hide()
-            self.LandmarksName[i].hide()
-        self.optimize.hide()
-        self.deleteButton.hide()
-        self.changeButton.hide()
-        self.PsContents.hide()
+        # for i in range(0,5,1):
+        #     self.names[i].hide()
+        #     self.contents[i].hide()
+        #     self.reviews[i].hide()
+        #     self.reviewPoints[i].hide()
+        #     self.reviewStars[i].hide()
+        #     self.imgs[i].hide()
+        #     self.no_imgs[i].hide()
+        #     self.LandmarksName[i].hide()
+        # self.optimize.hide()
+        # self.deleteButton.hide()
+        # self.changeButton.hide()
+        # self.PsContents.hide()
 
     #별점은 중대사항: 디폴트 지정에 맞춰 별 갯수 노출 세팅
         for i in range(0,5,1):
@@ -420,7 +421,7 @@ class Ui_MainWindow(QMainWindow):
 
         # 새 스레드에서 작업 완료되기 전까지 띄울 로딩 안내문
         self.SearchLoading.show()
-        #print("랜드마크: %s"%self.LandmarksName[0].text())
+        print("랜드마크: %s"%self.LandmarksName[0].text())
         while not self.SearchEnd:
             self.SearchLoading.setText("내용을 불러오고 있습니다.\n잠시만 기다려 주세요.")
             QtTest.QTest.qWait(1000)
@@ -428,7 +429,7 @@ class Ui_MainWindow(QMainWindow):
             QtTest.QTest.qWait(1000)
             self.SearchLoading.setText("내용을 불러오고 있습니다.\n잠시만 기다려 주세요...")
             QtTest.QTest.qWait(1000)
-        #print("랜드마크: %s"%self.LandmarksName[0].text())
+        print("랜드마크: %s"%self.LandmarksName[0].text())
 
         # 검색 끝났으면 SearchEnd를 false로 바꾸고 로딩안내문 숨김
         self.SearchEnd = False
@@ -597,7 +598,7 @@ class Ui_MainWindow(QMainWindow):
             
             self.names[i].setGeometry(QtCore.QRect(10, 8+195*i, 535, 31))
             self.contents[i].setGeometry(QtCore.QRect(145, 84+195*i, 418, 130))
-            self.imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 130))
+            self.imgs[i].setGeometry(QtCore.QRect(10, 42+195*i, 130, 100))
             self.reviewPoints[i].setGeometry(QtCore.QRect(144, 44+195*i, 51, 17))
             self.reviews[i].setGeometry(QtCore.QRect(258, 44+195*i, 101, 16))
             self.LandmarksName[i].setGeometry(QtCore.QRect(145, 64+195*i, 418, 16))
