01fa43893e185e71b9acbbe00d5249f60706da76
SangYoung-Choi <tkddud06@naver.com>
2023-06-13 00:32:16 +0900

    일부 수정사항 머지

--- apikey.py
+++
@@ -1,6 +0,0 @@
-
-OPENAI_API_KEY = "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"
-
-RapidAPI_KEY = "1b3cf7b441msh325c8cfa955e0edp163f9fjsnf4df3fa86e8b"
-
-GoogleMap_API_KEY="AIzaSyB8I74JlUYDKbZyDCQs2vAtelO9FrGKNGA"
