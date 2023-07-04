10cfcc6392ae2fe9ef2f95a619506870ad940a83
ProSet <101691695+tkddud06@users.noreply.github.com>
2023-07-04 18:18:00 +0900

    Update map.py

--- map.py
+++ map.py
@@ -1,10 +1,10 @@
-# this apache license 2.0 is applied only decode_polyline function.
+# apache license 2.0 is applied only decode_polyline function.
 # edit : The name of the variable has changed from 'points' to 'pointse' by tkddud06.
 # Copyright 2020 Google Inc. All rights reserved.
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
+# use this function except in compliance with the License. You may obtain a copy of
 # the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
