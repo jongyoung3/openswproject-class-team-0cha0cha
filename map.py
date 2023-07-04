f35ef19f1d0165dc0b4e6ab4267a8f557f4fd428
ProSet <101691695+tkddud06@users.noreply.github.com>
2023-07-04 17:40:12 +0900

    Update map.py

--- map.py
+++ map.py
@@ -1,3 +1,22 @@
+# this apache license 2.0 is applied only decode_polyline function.
+# edit : The name of the variable has changed from 'points' to 'pointse' by tkddud06.
+# Copyright 2020 Google Inc. All rights reserved.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+
 import googlemaps
 import folium
 import webbrowser
